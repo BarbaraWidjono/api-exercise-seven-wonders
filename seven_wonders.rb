60214aa9c3178f9912e602276a433263406d8e9e
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-30 12:34:00 -0700

    Helper method to build URL

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -2,6 +2,13 @@ require 'httparty'
 
 # Using Googles Geocoding API web service
 # https://maps.googleapis.com/maps/api/geocode/json?address=Great Pyramid of Giza&key=AIzaSyBP30mYnbwKpZ0lCHtp6FuvcNSjNG0GsGM
+def build_url(wonder)
+  google_api_key = "AIzaSyBP30mYnbwKpZ0lCHtp6FuvcNSjNG0GsGM"
+  geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{wonder}&key=#{google_api_key}"
+  encoded_url = URI.encode(geocode_url)
+  return encoded_url
+end
+
 
 
 seven_wonders = ["Great Pyramid of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus", ""]
@@ -13,12 +20,8 @@ gps_hash = {}
 seven_wonders.each do |wonder|
 
 
-  google_api_key = "AIzaSyBP30mYnbwKpZ0lCHtp6FuvcNSjNG0GsGM"
-  geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{wonder}&key=#{google_api_key}"
-  encoded_url = URI.encode(geocode_url)
+  encoded_url = build_url(wonder)
   response = HTTParty.get(encoded_url)
-
-
 
   if response.code == 200 && response['status'] == "OK"
     gps_hash["#{wonder}"] = response['results'][0]["geometry"]["location"]
